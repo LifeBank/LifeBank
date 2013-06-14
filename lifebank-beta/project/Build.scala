c8cb98136691a3a7da66148793a60ff1de6f952a
Victor Igbokwe <vicsstar@yahoo.com>
2013-06-14 17:07:05 +0100

    Finished the core of the app collecting the emails.

--- lifebank-beta/project/Build.scala
+++ lifebank-beta/project/Build.scala
@@ -10,12 +10,11 @@ object ApplicationBuild extends Build {
   val appDependencies = Seq(
     // Add your project dependencies here,
     jdbc,
-    anorm
+    anorm,
+    "mysql" % "mysql-connector-java" % "5.1.25"
   )
-
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here      
   )
-
 }
