5dd865cf8758fe5ca566987d45d0f67b5c60fdf8
Opeyemi Obembe <kehers@gmail.com>
2013-06-15 15:37:51 +0100

    Update

--- lib/config.php
+++ lib/config.php
@@ -3,7 +3,7 @@
  * Database Constants - these constants refer to 
  * the database configuration settings. 
  */
-
+session_start();
 require_once 'Database.php';
 
 $host = 'localhost';
@@ -15,5 +15,11 @@ Database::connect($host, $user, $pass, $database);
 
 
 //Define site root
+
+define("Token", "1511239506-z7ABQ0ov2TiE155CJKpF7IZxUsubMJZrLyNCoo8");
+define("Secret", "KEM6MfoczpUEan7XPRo9swr9G7PAmcbgi7YpmYLjJQ");
+define("TwitterOauthKey", "8TF5bADf85x8aIPVuuinJw");
+define("TwitterOauthSecret", "evsSLub5u0ifh9ZpClCbOM24ubOpBMxjBKxs4KOeZPI");
+define("TwitterCallback", "http://localhost/ray/lifebank/user/twitter-callback");
 define('SITEURL', 'http://localhost/lifebank');
 ?>
---
+++ views/user/twitter_callback.php
@@ -0,0 +1,54 @@
+<?php
+require 'lib/twitter/twitterOAuth.php';
+
+// returning from auth, create tokens and save
+if(isset($_REQUEST['oauth_token'])) {
+	// Create TwitterOAuth object with app key/secret and token key/secret from default phase
+	$twitter = new TwitterOAuth(TwitterOauthKey, TwitterOauthSecret, $_SESSION['twitter_oauth_request_token'], $_SESSION['twitter_oauth_request_token_secret']);
+
+	// Request access tokens from twitter
+	$tok = $twitter->getAccessToken($_REQUEST['oauth_verifier']);
+  
+  function get_twitter_user($tok) {
+      $twitter = new TwitterOAuth(TwitterOauthKey, TwitterOauthSecret, $tok['oauth_token'], $tok['oauth_token_secret']);
+      $args = array('screen_name' => $tok['screen_name'], 'skip_status' => 1);
+      return $twitter->get("http://api.twitter.com/1.1/users/show.json", $args);
+  }
+  
+	// Get timeline
+	if($tok['screen_name']) {
+    if ($user->is_logged()) {
+      // Logged? Add account
+      $data = get_twitter_user($tok);
+      $user->add_social_account($data['id'], $data['name'], 't', $tok['oauth_token'], $tok['oauth_token_secret'], $tok['profile_image_url']);
+      // Redirect to profile page
+      header('location:'.$_SESSION['user']['username']);
+      exit;
+    }
+    else {
+      // Nope? Login or Register
+      if ($user->social_login($tok['id'], 't', $tok['oauth_token'], $tok['oauth_token_secret'])) {
+        // Redirect to profile page
+        header('location:'.$_SESSION['user']['username']);
+        exit;
+      }
+      else {
+        // register
+        $data = get_twitter_user($tok);
+        $_SESSION['sm']['twitter'] = $data;
+        header('location:../complete-signup');
+        exit;
+      }
+    }
+    
+    
+    // Remove unecessary sessions
+    unset($_SESSION['twitter_oauth_request_token']);
+    unset($_SESSION['twitter_oauth_request_token_secret']);
+  }
+}
+else {
+	header('location:twitter');
+	exit;
+}
+?>
